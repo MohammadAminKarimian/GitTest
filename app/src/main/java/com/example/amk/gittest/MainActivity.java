87022c528f1980d27122f489b927c1b6f395ccb7
MohammadAminKarimian <42168296+MohammadAminKarimian@users.noreply.github.com>
2018-08-24 19:41:49 +0430

    Initial commit

---
+++ app/src/main/java/com/example/amk/gittest/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.amk.gittest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Step_1 Created on BETA-AMK
+
+
+    }
+}
